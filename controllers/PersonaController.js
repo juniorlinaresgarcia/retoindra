744dc4570fb50ca249ba7da41176f73b1c28a95e
Junior <jlinares0996@gmail.com>
2021-04-29 17:36:50 -0500

    commit 29_04_21

--- controllers/PersonaController.js
+++ controllers/PersonaController.js
@@ -1,7 +1,7 @@
 const AwsUtils = require("../common/helpers/AwsUtils");
 const PersonaService = require("../services/PersonaService");
 const Messages = require("../common/constants/messagesApi"); 
-
+//commit a pasar a master
 module.exports.listar = async (event, context, callback) => {
 
     try {
